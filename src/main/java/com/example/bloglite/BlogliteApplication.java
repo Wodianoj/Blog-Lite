52ada79958b18091973a1db153685b67ca4a877b
Rafał Mazurek <r.mazurek@nowodworskiestates.pl>
2022-03-18 22:03:13 +0100

    add project generated by Spring Initializr

---
+++ src/main/java/com/example/bloglite/BlogliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloglite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogliteApplication.class, args);
+	}
+
+}
