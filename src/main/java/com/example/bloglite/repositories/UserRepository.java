a476812ce3039ddd07df6102cba553c5feafe826
Rafał Mazurek <r.mazurek@nowodworskiestates.pl>
2022-06-22 23:14:57 +0200

    add entity BlTag, add prefix strategy to remove Bl from table

--- src/main/java/com/example/bloglite/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bloglite.repositories;
-
-import com.example.bloglite.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>
-{
-}
